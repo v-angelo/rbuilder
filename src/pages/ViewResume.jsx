d3fb9a063db604588e74ba87db569132537932e2
v-angelo <v.angelo003@gmail.com>
2026-03-16 11:24:01 +0530

    added get function on viewResume to fetch from json-sever db

--- src/pages/ViewResume.jsx
+++ src/pages/ViewResume.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaBackward, FaFileDownload } from 'react-icons/fa';
 import { IoMdRefresh } from 'react-icons/io';
 import Preview from '../components/Preview';
 import Edit from '../components/Edit';
+import { useParams } from 'react-router-dom';
+import { getResumeAPI } from '../services/allApiService';
 
 function viewResume() {
+
+  const {id} = useParams();
+  const [resumeData, setResumeData] = useState({});
+  console.log(resumeData);
+  
+  const getResumeDetails = async () => {
+    if (id) {
+      const result = await getResumeAPI(id);
+      console.log(result.data);
+      setResumeData(result.data);      
+    }
+  }
+
+  useEffect (() => {
+    getResumeDetails();
+  }, []);
+
   return (
     <section className='max-w-4xl mx-auto'>
       <article className='max-w-lg mx-auto flex justify-evenly items-center text-3xl my-10'>
@@ -15,7 +34,7 @@ function viewResume() {
       </article>
 
       <article>
-        <Preview />
+        <Preview resumeData={resumeData} />
       </article>
     </section>
   );
--- src/services/allApiService.js
+++ src/services/allApiService.js
@@ -1,6 +1,11 @@
 import apiService from '../api/apiService';
 
-// add resume api called by user input
+// add resume api called by user input when finished button clicked
 export const addResumeAPI = async (resumeData) => {
    return await apiService("POST", "/allResumes", resumeData);
+}
+
+// get resume api called by viewResume when page loads
+export const getResumeAPI = async (id) => {
+   return await apiService("GET", `/allResumes/${id}`, {});
 }
